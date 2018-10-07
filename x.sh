a6021bc0b028d18417195a1d715fa3f5b9949f7b
rawri <37624066+rawri@users.noreply.github.com>
2018-10-07 11:42:38 +0200

    Update x.sh

--- x.sh
+++ x.sh
@@ -1,2 +1,2 @@
 sudo pacman -Sy
-sudo pacman -S base-devel xorg-server xorg-xdpyinfo xorg-xinitttf-linux-libertine cronie dunst ffmpeg firefox gnome-keyring mpd mpv ncmpcpp networkmanager network-manager-applet newsboat pulseaudio pulseaudio-alsa pamixer pulsemixer python-pywal scrot youtube-dl youtube-viewer mediainfo dmenu htop guake 
+sudo pacman -S base-devel xorg-server xorg-xdpyinfo xorg-xinit ttf-linux-libertine cronie dunst ffmpeg firefox gnome-keyring networkmanager network-manager-applet newsboat pulseaudio pulseaudio-alsa pamixer pulsemixer python-pywal scrot youtube-dl youtube-viewer mediainfo dmenu htop guake obmenu feh tint2 openbox

5f3dddde8ecb414de2453481190492a4f6b4fbac
rawri <37624066+rawri@users.noreply.github.com>
2018-10-12 10:32:17 +0200

    Delete x.sh

--- x.sh
+++
@@ -1,2 +0,0 @@
-sudo pacman -Sy
-sudo pacman -S base-devel xorg-server xorg-xdpyinfo xorg-xinit ttf-linux-libertine cronie dunst ffmpeg firefox gnome-keyring networkmanager network-manager-applet newsboat pulseaudio pulseaudio-alsa pamixer pulsemixer python-pywal scrot youtube-dl youtube-viewer mediainfo dmenu htop guake obmenu feh tint2 openbox
